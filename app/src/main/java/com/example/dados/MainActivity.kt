d5f5a114b5ed047f27dd792c75f8ced985e2ae84
Aluno <@ninaazevedo0202>
2023-05-02 15:09:23 -0300

    atualizaçoes 02/05

--- app/src/main/java/com/example/dados/MainActivity.kt
+++ app/src/main/java/com/example/dados/MainActivity.kt
@@ -13,6 +13,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -47,7 +51,7 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun RolaDados(){
-    var resultado=1
+    var resultado by remember { mutableStateOf(1) }
     Text(
         text ="Gire o dado",
         modifier = Modifier.fillMaxWidth(),
@@ -55,33 +59,39 @@ fun RolaDados(){
         textAlign = TextAlign.Center,
         fontWeight = FontWeight.Bold,
         color = Color.Black
-
     )
 
-
-
+        val imageResource = when (resultado) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
 
-    ) { Image(
-        painter =  painterResource(id =  R.drawable.dice_1),
+    ) {
+        Image(
+        painter =  painterResource(id = imageResource),
         contentDescription =null
     )
         Button(
             onClick = {
                 resultado=(1..6).random()
-            }
+               }
         ) {
             Text(
                 text ="Botão" )
         }
 
-         }
+    }
 
-        }
+}
 
 
 
